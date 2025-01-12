55733cdd1308600bb72da378bcd442a5acb59b42
Alejo <fernandezalejo981@gmail.com>
2025-01-12 17:54:21 -0300

    obras index

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
+const slider = document.querySelector('.slider');
 const container = document.querySelector('.container');
-document.querySelector('.slider').addEventListener('input', (e) => {
-  container.style.setProperty('--position', `${e.target.value}%`);
-})
+
+slider.addEventListener('input', (e) => {
+  const value = `${e.target.value}%`;
+  container.style.setProperty('--position', value);
+});
